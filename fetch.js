3f90fc182b02a2848f12ed74cdf231d4e0fb348b
Elfving2 <112498823+Elfving2@users.noreply.github.com>
2024-04-24 08:39:54 +0200

    fixed some problems

--- fetch.js
+++ fetch.js
@@ -1,37 +1,66 @@
 const container = document.querySelector(".container");
 
-async function fetchImage() {
-  var divList = [];
+async function fetchUrls() {
   const response = await fetch("https://dog.ceo/api/breeds/image/random/9");
-  var result = await response.json();
+  const result = await response.json();
+  console.table(result);
+  return result.message;
+}
 
-  for (var i = 0; i < 9; i++) {
-    var imageContainer = document.createElement("div");
-    var image = document.createElement("img");
+async function populateDiv() {
+  const urlList = await fetchUrls();
+
+  urlList.forEach(element => {
+    const imageContainer = document.createElement("div");
+    const image = document.createElement("img");
     image.classList.add("image");
-    image.src = result.message[i];
+    image.src = element;
     imageContainer.classList.add("imageContainer");
     imageContainer.appendChild(image);
     container.appendChild(imageContainer);
-    divList.push(imageContainer);
-  }
-  return divList;
+  });
 }
 
-async function printDivList() {
-  try {
-    const divList = await fetchImage();
-    divList.forEach((div) => {
-      div.addEventListener("click", () => {
-        div.classList.add("card-back");
-        div.childNodes[0].src = "";
-      });
-    });
-  } catch (error) {
-    console.error(error);
-  }
+function ImageContainerClick() {
+  container.childNodes.forEach(element => {
+    element.addEventListener("click", () => {
+      element.classList.add("card-back");
+      element.childNodes[0].src = "";
+    })
+  })
 }
 
-printDivList();
+async function AddClickFunction() {
+  await populateDiv();
+  ImageContainerClick();
+}
+
+AddClickFunction();
+
+
+
+
+
+
+
+
+
+// return divList;
+
+// async function printDivList() {
+//   try {
+//     const divList = await fetchImage();
+//     divList.forEach((div) => {
+//       div.addEventListener("click", () => {
+//         div.classList.add("card-back");
+//         div.childNodes[0].src = "";
+//       });
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// printDivList();
 
 
