bd27145dc09f7a88d7742750098b4834b0e288ff
sebastian elfving <sebastian.elfving@hotmail.com>
2024-04-24 18:24:25 +0200

    there are some bugs

--- fetch.js
+++ fetch.js
@@ -1,21 +1,16 @@
-
 const container = document.querySelector(".container");
 
-  function populateDiv(array) {
+function populateDiv(array) {
   for (let i = 0; i < array.length; i++) {
-    const imageContainer = document.createElement("div");
     const card = document.createElement("div");
+    const flipCard = document.createElement("div");
     const cardInner = document.createElement("div");
     const cardFront = document.createElement("div");
     const cardBack = document.createElement("div");
     const imageFront = document.createElement("img");
     const imageBack = document.createElement("img");
 
-    imageContainer.classList.add("imageContainer");
-    card.classList.add("flip-card");
-    cardInner.classList.add("flip-card-inner");
-    cardFront.classList.add("flip-card-front");
-    cardBack.classList.add("flip-card-back");
+    card.classList.add("card");
     imageFront.classList.add("image");
     imageBack.classList.add("image");
     
@@ -23,10 +18,13 @@ const container = document.querySelector(".container");
 
     imageFront.src = "./images/cardback.png";
     imageBack.src = array[i];
-
-    container.appendChild(imageContainer);
-    imageContainer.appendChild(card);
-    card.appendChild(cardInner);
+    flipCard.classList.add("flip-card");
+    cardInner.classList.add("flip-card-inner");
+    cardFront.classList.add("flip-card-front");
+    cardBack.classList.add("flip-card-back");
+    container.appendChild(card);
+    card.appendChild(flipCard);
+    flipCard.appendChild(cardInner);
     cardInner.appendChild(cardFront);
     cardInner.appendChild(cardBack);
     cardFront.appendChild(imageFront);
@@ -58,61 +56,48 @@ async function shuffleImages() {
 async function Run() {
   array = await shuffleImages();
   populateDiv(array);
+  OnClickCard();
+}
+
+function OnClickCard() {
+  const cards = document.querySelectorAll(".card");
+  let hashMap = new Map();
+  let array = [];
+  cards.forEach(card => {
+      card.addEventListener("click", () => {
+        const flipCard = card.querySelector(".flip-card");
+        const flipCardInner = flipCard.querySelector(".flip-card-inner");
+        const flipCardBack = flipCardInner.querySelector(".flip-card-back");
+        const image = flipCardBack.querySelector(".image");
+        console.log(array);
+        if (array.length != 2) { 
+          flipCard.classList.add("flip-card-motion");
+          flipCardInner.classList.add("flip-card-motion");
+          array.push(image.src);
+          hashMap.set(image.src, flipCardInner);
+        }
+        setTimeout(() => {
+          if (array.length == 2) {
+            if (array[0] == array[1]) {
+              //keep them flipped? 
+              console.log("CORRECT");
+            } else {
+              // flip cards back
+              const first = hashMap.get(array[0]);
+              const second = hashMap.get(array[1]);
+              
+              first.classList.add("flip-card-motion-back");
+              second.classList.add("flip-card-motion-back");
+            }
+            array = [];
+            console.log(array.length);
+            console.log(hashMap);
+        } 
+        }, 1500);
+      });   
+  })
 }
 
 Run();
-
-// function ImageContainerClick() {
-//   container.childNodes.forEach(element => {
-//     element.addEventListener("click", () => {
-
-//     })
-//   })
-// }
-//ImageContainerClick();
-
-// AddClickFunction();
-
-
-
-
-/*
-<div class="flip-card">
-  <div class="flip-card-inner">
-    <div class="flip-card-front">
-      <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
-    </div>
-    <div class="flip-card-back">
-      <h1>John Doe</h1>
-      <p>Architect & Engineer</p>
-      <p>We love that guy</p>
-    </div>
-  </div>
-</div>
-
-*/
-
-
-
-
-
-
-// return divList;
-
-// async function printDivList() {
-//   try {
-//     const divList = await fetchImage();
-//     divList.forEach((div) => {
-//       div.addEventListener("click", () => {
-//         div.classList.add("card-back");
-//         div.childNodes[0].src = "";
-//       });
-//     });
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// printDivList();
 
 
